4bdb8cb97d5184714939be7cefb0b86b676a254c
Blake Neely <blakeneely21@gmail.com>
2020-02-26 16:23:30 -0600

    added Axios and built weather API call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import './App.css';
+import Weather from './components/weather/Weather'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React Weather App</h1>
+import './App.css';
+import 'weather-icons/css/weather-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Axios from 'axios';
+
+const API_key = 'a1e3b3c298370700698105fb435bbe64';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={};
+    this.getWeather();
+  }
+
+  getWeather = () => {
+    Axios.get(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`)
+    .then(function(response){
+      console.log(response)
+    });
+  };
+
+  render(){
+    return(
+      <div className='App'>
+      <Weather />
     </div>
-  );
-}
+    )
+  };
+};
 
 export default App;
---
+++ src/components/weather/Weather.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Weather = () => {
+    return (
+        <div className="container">
+            <div className="cards">
+                <h1>Nashville</h1>
+                <h5 className="py-4">
+                    <i className="wi wi-day-sunny display-1"></i>
+                </h5>
+                <h1 className="py-2">52&deg;</h1>
+
+                {/* Show high and low temp */}
+                {highLowTemp(24, 19)}
+
+                <h4 className="py-4">Light Rain</h4>
+            </div> 
+        </div>
+    );
+};
+
+function highLowTemp(low, high) {
+    return(
+        <h3>
+            <span className="px-4">{low}&deg;</span>
+            <span className="px-4">{high}&deg;</span>
+        </h3>
+    );
+};
+
+export default Weather;
