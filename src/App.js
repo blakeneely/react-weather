8af4171764181b0f0b538c64e9215bbacbc168a9
Blake Neely <blakeneely21@gmail.com>
2020-02-26 16:40:09 -0600

    changed from Axios to async fetch request

--- src/App.js
+++ src/App.js
@@ -4,28 +4,39 @@ import Weather from './components/weather/Weather'
 import './App.css';
 import 'weather-icons/css/weather-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Axios from 'axios';
 
 const API_key = 'a1e3b3c298370700698105fb435bbe64';
 
 class App extends React.Component {
   constructor(){
     super();
-    this.state={};
+    this.state={
+      city: undefined,
+      country: undefined
+    };
     this.getWeather();
   }
 
-  getWeather = () => {
-    Axios.get(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`)
-    .then(function(response){
-      console.log(response)
-    });
-  };
+  getWeather = async() => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
+
+    const response = await api_call.json()
+
+    console.log(response)
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country
+    })
+  }
 
   render(){
     return(
       <div className='App'>
-      <Weather />
+      <Weather 
+        city={this.state.city} 
+        country={this.state.country}
+      />
     </div>
     )
   };
